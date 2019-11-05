07f724aad7673d5ab8f06522a743fc2b209f8369
lyndsiWilliams <kcatgirl@gmail.com>
2019-11-05 16:57:50 -0600

    Got Special buttons displaying

--- src/components/ButtonComponents/SpecialButtons/SpecialButton.js
+++ src/components/ButtonComponents/SpecialButtons/SpecialButton.js
@@ -1,9 +1,15 @@
 import React from "react";
 
-const SpecialButton = () => {
+const SpecialButton = (props) => {
+  const {spec} = props;
+  console.log(spec);
+
   return (
     <>
       {/* Display a button element rendering the data being passed down from the parent container on props */}
+      <div>
+        <button>{spec}</button>
+      </div>
     </>
   );
 };
--- src/components/ButtonComponents/SpecialButtons/Specials.js
+++ src/components/ButtonComponents/SpecialButtons/Specials.js
@@ -8,14 +8,17 @@ import SpecialButton from "./SpecialButton";
 
 const Specials = () => {
   // STEP 2 - add the imported data to state
-  const [special, setSpecial] = useState(specials);
-  
+  const [special] = useState(specials);
+  console.log(special);
 
   return (
     <div>
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
+       {special.map((spec, i) => (
+         <SpecialButton key = {i} spec = {spec} />
+       ))}
     </div>
   );
 };
